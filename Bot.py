09fb976a32bb50900d19db749a6b67febdbf7738
Arcade-Master-Team <arcade.master.team@gmail.com>
2025-10-29 21:25:06 -0300

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,42 @@
+import discord
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$Cat'):
+        await message.channel.send("Yes")
+    elif message.content.startswith('$Speak'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send(message.content)
+
+
+
+import random
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+client.run("Token")
