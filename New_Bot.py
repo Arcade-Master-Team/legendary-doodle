1e1b9ec8b90316cfc5195b036e760363a00bacd1
Arcade-Master-Team <arcade.master.team@gmail.com>
2025-11-05 20:55:47 -0300

    New_Bot.py

---
+++ New_Bot.py
@@ -0,0 +1,47 @@
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def bienvenida(ctx):
+    await ctx.send("Bienvenido a mi servidor!")
+
+@bot.command()
+async def dircall(ctx):
+    await ctx.send(f"Hola{ctx.author.mention}, bienvenido")
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def add(ctx, left: float, right: float):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run("Token")
